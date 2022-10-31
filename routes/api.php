a93a3ed8cc582c357d95db6a0dd156fb72e87ce1
Aripin23 <ilhamarifin.official@gmail.com>
2022-10-31 22:43:12 +0700

    menambahkan methode delete untuk data student

--- routes/api.php
+++ routes/api.php
@@ -44,3 +44,6 @@ Route::post('/student', [StudentController::class, 'store']);
 #methode put
 Route::put('/student/{id}', [StudentController::class, 'update']);
 
+#delete data student dengan id
+#methode delete
+Route::delete('/student/{id}', [StudentController::class, 'destroy']);
