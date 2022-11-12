2b3eb732ef15db8712a761cad79bd08fba18a97f
Aripin23 <ilhamarifin.official@gmail.com>
2022-11-12 11:19:09 +0700

    menambahkan routes api StudentController dan merapihkan route berdasarkan route masing-masing

--- routes/api.php
+++ routes/api.php
@@ -20,30 +20,30 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
+// Routes all form AnimalControlles
 #methode get
 Route::get('/animals', [AnimalController::class, 'index']);
-
 #methode post
 Route::post('/animals', [AnimalController::class, 'store']);
-
 #methode put
 Route::put('/animals/{id}', [AnimalController::class, 'update']);
-
 #methode delete
 Route::delete('/animals/{id}', [AnimalController::class, 'destroy']);
 
+// Routes all form StudentControlles
 #get all resorce student
 #methode get
 Route::get('/student', [StudentController::class, 'index']);
-
 #menambahkan resorce student
 #methode post
 Route::post('/student', [StudentController::class, 'store']);
-
 #update resource student
 #methode put
 Route::put('/student/{id}', [StudentController::class, 'update']);
-
 #delete data student dengan id
 #methode delete
 Route::delete('/student/{id}', [StudentController::class, 'destroy']);
+#show detail resource student
+#methode get (show)
+Route::get('/student/{id}', [StudentController::class, 'show']);
