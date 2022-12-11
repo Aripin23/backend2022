36abc1529c8d475aeb9060c930047b160f0de869
Aripin23 <ilhamarifin.official@gmail.com>
2022-12-11 08:14:47 +0700

    ini adalah pertemuan_10 synchronous dan asynchronous

---
+++ synchronous.js
@@ -0,0 +1,9 @@
+console.log("Aripin Membuka Bowser Google Chrome.");
+
+console.log(`
+Aripin download file selama 1 jam.
+  Downloading 1 Hour ...
+  Download Complete.
+`);
+
+console.log("Aripin membuka Youtube.");
